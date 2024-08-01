24fe6ae4b8a472a6b0ac0eb1ca08faa7a7ed9719
Udynathcodes <uwahuduakobong@gmail.com>
2024-08-01 20:56:36 +0100

    Update file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+A function that adds two values
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two floats"""
+    return a + b
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""converting a float to str"""
+
+
+def to_str(n: float) -> str:
+    """returns the string representation of the float"""
+    return str(n)
